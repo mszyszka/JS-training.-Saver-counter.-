a79635e9e017ba6cdfdc221ab88c783a1d94f14c
Michał Szyszka <mszyszka223@gmail.com>
2018-01-18 14:12:01 +0100

    I solved token problem. It was about wrong typing before token

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-///////////////////////decrease in the value of the car/////////////////////////////////
+
 var carCost = 10000;
 var carAge = 0;
 var timeOfUse = 10;
@@ -6,81 +6,63 @@ var timeOfUse = 10;
 var carValueAfterUsing;
 
 
-function valueDecrese(carCost, carAge, timeOfUse){
+function valueDecrese(timeOfUse, carAge, carCost) {
     
-    for (var i = 0; i < timeOfUse; i ++){
-        
-        if(carAge==0)
-        {
-            if(i==0)
-            {
-                 carValueAfterUsing = carCost * 70%;
+    
+    for (var i = 0; i < timeOfUse; i++) {
+        if (carAge == 0) {
+            if (i == 0) {
+                 carValueAfterUsing = carCost * "0.7" ;
             }
-            else if (i==1)
-            {
-                  carValueAfterUsing = carValueAfterUsing * 90%;
+            else if (i == 1) {
+                 carValueAfterUsing =  carValueAfterUsing * "0.9" ;
             }
-            else 
-            {
-                  carValueAfterUsing = carValueAfterUsing * 94%;
+            else {
+                 carValueAfterUsing =  carValueAfterUsing * "0.94" ;
             }
         }
-            
-        else if(carAge==1)
-        {
-            if(i==0)
-            {
-                 carValueAfterUsing = carCost * 90%;
+        else if (carAge == 1) {
+            if (i == 0) {
+                carValueAfterUsing = carCost * "0.9" ;
             }
-            else if(i==1)
-            {
-                  carValueAfterUsing = carValueAfterUsing * 90%;
+            else if (i == 1) {
+                carValueAfterUsing = carValueAfterUsing * "0.9" ;
             }
-            else 
-            {
-                carValueAfterUsing = carValueAfterUsing * 94%;
-            }
-
-        }
-        
-        else if(carAge==2)
-        {
-            if(i==0)
-            {
-                 carValueAfterUsing = carCost * 90%;
-            }
-            else 
-            {
-                  carValueAfterUsing = carValueAfterUsing * 94%;
-            }
-        
-        }
-        
-        else 
-        {
-            if(i==0)
-            {
-                carValueAfterUsing = carCost * 94%;
-            }
-            else
-            {
-                carValueAfterUsing = carValueAfterUsing *94%;
+            else {
+                carValueAfterUsing = carValueAfterUsing * "0.94" ;
             }
         }
-            
-        
+        else if (carAge == 2) {
+            if (i == 0) {
+                carValueAfterUsing = carCost * "0.9" ;
+            }
+            else {
+                carValueAfterUsing = carValueAfterUsing * "0.94" ;
+            }
+        }
+        else {
+            if (i == 0) {
+                carValueAfterUsing = carCost * "0.94" ;
+            }
+            else {
+                carValueAfterUsing = carValueAfterUsing * "0.94" ;
+            }
+        }
     }
-    
-    return carValueAfterUsing;
-
+    alert(carValueAfterUsing);
 }
 
-
-document.getElementById("valueDecreseBtn").addEventListener("click", valueDecrese, false);
-
-alert(valueDecrese);
+var decrese = valueDecrese(10, 1, 50000);
 
 
+    
+    
+    
 
+
+        
+        
+        
+//    return carValueAfterUsing;
 
 ////////////////////////////////////////////////////////////////////////////////////////
